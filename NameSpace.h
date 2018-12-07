fc7dc4890f90a51c42efa63486a4c9ca27a8f912
Qt Li <tqjxlm@gmail.com>
2018-12-08 04:48:30 +0800

    Fix AddModel plugin and use MapNode to manage models

--- NameSpace.h
+++ NameSpace.h
@@ -3,17 +3,17 @@
 
 #define MAX_SUBVIEW 2
 
-// Define window splits visibility used in node mask
-// and viewer widget
-enum WindowVisibility {
-	SHOW_IN_NO_WINDOW = 0x0fffffff,
-	SHOW_IN_WINDOW_1 = 0x10000000,
-};
-
 // Define what to ignore in node traversal
 enum TraversalOption {
 	INTERSECT_IGNORE = 0x00000004,
 	TRAVERSAL_IGNORE = 0x00000010
+};
+
+// Define window splits visibility used in node mask
+// and viewer widget
+enum WindowVisibility {
+  SHOW_IN_NO_WINDOW = 0x0fffffff,
+  SHOW_IN_WINDOW_1 = 0x10000000,
 };
 
 #endif
