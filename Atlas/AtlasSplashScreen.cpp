d90e923c6b90875447ba9d3b5c77941e7697955b
Qt Li <tqjxlm@gmail.com>
2018-12-07 11:51:53 +0800

    Fix splash screen Ui

--- Atlas/AtlasSplashScreen.cpp
+++ Atlas/AtlasSplashScreen.cpp
@@ -2,23 +2,23 @@
 
 AtlasSplashScreen::AtlasSplashScreen(QPixmap& pixmap)
 {
-    int imageSize = 350;
-    int margin = 20;
+  int imageSize = 350;
+  int margin = 20;
 
-	nowStep = 0; 
-	progressBar = new QProgressBar(this);
-	progressBar->setGeometry(margin, imageSize-margin, imageSize, 300);
-    progressBar->setTextVisible(false);
+  nowStep = 0;
+  progressBar = new QProgressBar(this);
+  progressBar->setGeometry(margin, imageSize - margin, imageSize - margin, margin);
+  progressBar->setTextVisible(false);
 
-	rePixmap = pixmap.scaledToHeight(imageSize, Qt::SmoothTransformation);
-	this->setPixmap(rePixmap);
+  rePixmap = pixmap.scaledToHeight(imageSize, Qt::SmoothTransformation);
+  this->setPixmap(rePixmap);
 
-	splashFont.setFamily("Arial");
-	splashFont.setBold(true);
-	splashFont.setPixelSize(15);
-	splashFont.setStretch(125);
+  splashFont.setFamily("Arial");
+  splashFont.setBold(true);
+  splashFont.setPixelSize(15);
+  splashFont.setStretch(125);
 
-	this->setFont(splashFont);
+  this->setFont(splashFont);
 
 }
 
@@ -27,14 +27,14 @@ AtlasSplashScreen::~AtlasSplashScreen()
 }
 
 void AtlasSplashScreen::setTotalInitSteps(int num) {
-	progressBar->setRange(0, num);
-	totalSteps = num;
+  progressBar->setRange(0, num);
+  totalSteps = num;
 }
 
 void AtlasSplashScreen::setNowInitName(const QString& name)
 {
-	nowStep++;
-	progressBar->setValue(nowStep);
-	this->showMessage(tr("%1\n").arg(name), 
-        Qt::AlignHCenter | Qt::AlignBottom, Qt::white);
+  nowStep++;
+  progressBar->setValue(nowStep);
+  this->showMessage(tr("%1\n").arg(name),
+    Qt::AlignHCenter | Qt::AlignBottom, Qt::white);
 }
